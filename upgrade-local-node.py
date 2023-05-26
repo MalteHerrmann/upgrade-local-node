a07a8204d23196cc3c8d4d6fe8806da20060c69b
MalteHerrmann <malte@evmos.org>
2023-05-26 10:04:31 +0200

    fix commands

--- upgrade-local-node.py
+++ upgrade-local-node.py
@@ -14,30 +14,31 @@ import time
 
 
 EVMOSD_HOME = os.path.join(os.path.expanduser("~"), ".tmp-evmosd")
-DEFAULT_FLAGS = "--home ~/.tmp-evmosd --chain-id evmos_9000-1 --keyring-backend test --gas auto --fees 100000000000000aevmos --gas-adjustment 1.5 -b block -y"
-UPGRADE_PROPOSAL = "evmosd tx gov submit-proposal software-upgrade --title 'Upgrade to v0.1.0' --description 'Upgrade to v0.1.0' --upgrade-height 0 --deposit 100000000000000000000000000aevmos"
+DEFAULT_FLAGS = "--home ~/.tmp-evmosd --chain-id evmos_9000-1 --keyring-backend test --gas auto --fees 100000000000000000aevmos --gas-adjustment 1.3 -b block -y"
 
 
 def build_upgrade_proposal(target_version: str, upgrade_height: int) -> str:
     """
     Builds the command string to submit a software upgrade proposal.
     """
-    command = "evmosd tx gov submit-proposal software-upgrade"
+    command = f"evmosd tx gov submit-legacy-proposal software-upgrade {target_version}"
     title = f"--title 'Upgrade to {target_version}'"
     description = f"--description 'Upgrade to {target_version}'"
-    upgrade_height = "--upgrade-height 0"
-    deposit = "--deposit 100000000000000000000000000aevmos"
+    upgrade_height = f"--upgrade-height {upgrade_height}"
+    deposit = "--deposit 100000000000000000000aevmos"
 
-    return f"{command} {title} {description} {upgrade_height} {deposit}"
+    return f"{command} {title} {description} {upgrade_height} {deposit} --no-validate"
 
 
-def execute_shell_command(command: str, home: str=EVMOSD_HOME, sender: str="dev0") -> str:
+def execute_shell_command(command: str, home: str=EVMOSD_HOME, sender: str="dev0", defaults: bool=True) -> str:
     """
     Runs a subprocess to execute the given shell command.
     """
     home_flag = f" --home {home}"
     sender_flag = f" --from {sender}"
     full_command = command + home_flag + sender_flag
+    if defaults:
+        full_command += " " + DEFAULT_FLAGS
 
     try:
         output = subprocess.check_output(full_command, shell=True, universal_newlines=True)
